6951bd7c42154e56c0473ea8e98d9f6d98574bba
sebitas0623 <sebastian.gomez3@udea.edu.co>
2021-01-18 01:57:29 +0000

    modelUpdate

--- ML_model/API_REST_Clasificador.py
+++ ML_model/API_REST_Clasificador.py
@@ -2,29 +2,46 @@ from bottle import Bottle, run, request
 import json
 import numpy as np
 from sklearn.externals import joblib
+import time
+import csv
 
 
 app = Bottle()
 aciertosT = 0;
 total = 0;
+TotReq = 0;
 
 @app.route('/clasificar', method='POST')
 def alarma():
 	global clf
 	global normalizador
-	global mean
-	global std
 	global aciertosT
 	global total
+	global TotReq
 
+	T_ini = int(round(time.time() * 1000)) # TimeStamp inicial en milisegundos
 	r = json.load(request.body) # se extrae el body de la peticion POST
-	#print(r, len(r))
+	#print("prueba: ", r, len(r))
 	r = np.matrix(r, dtype=np.float64)
-	features = r[:,0:4]
-	tags = np.array(r[:,4].T,dtype=int)
+	L = r.shape
 
-	features_N = (features-mean)/(std) # se normalizan las features
+	#tiempos[0,0] -> timestamp ; tiempos[0,1] -> TimeSpentONOS
+	tiempos = r[L[0]-1,:]
+	#print("tiempos:", tiempos)
+
+	#--- se extrae toda la matriz menos la ultima fila y columna
+	features = r[0:L[0]-1,0:L[1]-1]
+	#print("features: ",features)
+
+	#--- se extrae la ultima columna sin el ultimo valor.
+	tags = np.array(r[0:L[0]-1,L[1]-1].T,dtype=int)
+	#print("tags:",tags)
+
+	features_N = estandarizador.transform(features)
 	y = clf.predict(features_N) # clasificador
+
+	T_fin = int(round(time.time() * 1000)) # TimeStamp final en milisegundos
+	print("Time spent Classifier: ",T_fin - T_ini)
 
 	trues = 0
 	tam = len(y)
@@ -36,10 +53,19 @@ def alarma():
 	aciertosT += trues
 	total += tam 
 
-	print("Classificación: ",y)
+	print("Classificación:",y)
 	print("Reales: ", tags)
 	print("aciertos: ", aciertosT, "desaciertos: ",total - aciertosT, "accuracy: ",aciertosT/total)
 	print("--------------------------------------------------")
+
+	data = [TotReq, tiempos[0,1], T_fin - T_ini, T_fin - tiempos[0,0], tiempos[0,2]]
+	archivo = open("times.csv","a",)
+	salida = csv.writer(archivo)
+	salida.writerow(data)
+	del salida
+	archivo.close()
+
+	TotReq += 1
 
 
 def run():
@@ -52,20 +78,23 @@ def run():
 
 if __name__ == "__main__":
 
+	#--------------- Crea o sobreescribe el archivo -----------------------
+	archivo = open("times.csv","w")
+	salida = csv.writer(archivo)
+	salida.writerow(['#', 'TimeSpentONOS', 'TimeSpentClassif', 'TimeSpentTot','FlowPerPkt'])
+	del salida
+	archivo.close()
+
 	#------------------------ Initializar modelo ML --------------------------------
 	# cargar la matriz con las medias y desviacion estandar del conjunto de entrenamiento
-	#estandarizadores = joblib.load('SVM_stand.joblib') # del modelo svm
 	
-	estandarizadores = joblib.load('RF_Stand.joblib') # del modelo RF
-	estandarizadores = np.concatenate((estandarizadores[0], estandarizadores[1]), axis=0)
-	print(estandarizadores) 
-	
-	mean = estandarizadores[0,:]
-	std = estandarizadores[1,:]
+	#estandarizador = joblib.load('KNN_StandSet1_W60_Impar.joblib')
+	estandarizador = joblib.load('RF_StandSet1_W40_Impar.joblib')
+	print(estandarizador) 
 
 	#cargar el modelo de machine learning
-	#clf = joblib.load('SVM_model.joblib') # del modelo svm
-	clf = joblib.load('RF_model.joblib') # del modelo RF
+	#clf = joblib.load('KNN_modelSet1_W60_Impar.joblib') # del modelo RF
+	clf = joblib.load('RF_modelSet1_W40_Impar.joblib')
 	print("-------------------- Modelo ML -------------------")
 	print(clf)
 	print("--------------------------------------------------")
